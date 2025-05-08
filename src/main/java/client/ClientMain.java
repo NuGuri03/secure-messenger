2357ba842b14441380ef4d99b234b4d80f83be28
권혁주 <toturak1@gmail.com>
2025-05-08 12:20:30 +0900

    adas

--- src/main/java/client/ClientMain.java
+++ src/main/java/client/ClientMain.java
@@ -3,5 +3,7 @@ package client;
 public class ClientMain {
     public static void main(String[] args) {
         System.out.println("✅ Client started successfully!");
+        System.out.println("✅ Client started successfully!");
+        System.out.println("✅ Client started successfully!");
     }
 }
