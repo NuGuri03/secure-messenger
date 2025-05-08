daf0826190835303324f31e566a320e19da185eb
ssb <tomorrowmgm1314@gmaiil.com>
2025-05-08 11:59:58 +0900

    test

--- src/main/java/client/ClientMain.java
+++ src/main/java/client/ClientMain.java
@@ -3,5 +3,9 @@ package client;
 public class ClientMain {
     public static void main(String[] args) {
         System.out.println("✅ Client started successfully!");
+        System.out.println("✅ Client started successfully!");
+        System.out.println("✅ Client started successfully!");
+        System.out.println("✅ Client started successfully!");
+        System.out.println("✅ Client started successfully!");
     }
 }
