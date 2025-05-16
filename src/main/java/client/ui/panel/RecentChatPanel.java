78da1b76a05578c3fc5f0ee339873ec5dd1e4588
NuGuri03 <sym2596@knu.ac.kr>
2025-05-16 19:27:14 +0900

    feat: 제목 추가

--- src/main/java/client/ui/panel/RecentChatPanel.java
+++ src/main/java/client/ui/panel/RecentChatPanel.java
@@ -3,17 +3,28 @@ package client.ui.panel;
 import client.ui.ChatUI;
 
 import javax.swing.*;
+import javax.swing.border.MatteBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 
 public class RecentChatPanel extends JPanel {
+    private static final int PADDING = 30;
+
     public RecentChatPanel() {
         setLayout(new BorderLayout());
 
-        JLabel label = new JLabel("Recent");
-        label.setHorizontalAlignment(SwingConstants.CENTER);
-        label.setVerticalAlignment(SwingConstants.CENTER);
-        add(label, BorderLayout.NORTH);
+        JPanel title = new JPanel();
+        title.setLayout(new BoxLayout(title, BoxLayout.Y_AXIS));
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JLabel titleLabel = new JLabel("나의 최근 채팅");
+        titleLabel.setFont(new Font("Pretendard", Font.BOLD, 14));
+        titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        titleLabel.setBorder(BorderFactory.createEmptyBorder(PADDING / 6, 0, PADDING / 6, 0));
+
+        title.add(titleLabel);
+        title.setBorder(new MatteBorder(1, 1, 1, 1, Color.decode("#A9A9A9")));
+        add(title, BorderLayout.NORTH);
 
         JButton openChatButton = new JButton("채팅창 열기");
         openChatButton.addActionListener((ActionEvent e) -> {
