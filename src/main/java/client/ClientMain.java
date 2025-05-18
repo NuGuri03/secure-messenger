782ebf723de91da4531d6eba4172b9fa1e87018d
권혁주 <toturak1@gmail.com>
2025-05-18 19:33:45 +0900

    feat: LoginUI로 시작

--- src/main/java/client/ClientMain.java
+++ src/main/java/client/ClientMain.java
@@ -11,9 +11,8 @@ import client.ui.MainUI;
 
 public class ClientMain {
     public static void main(String[] args) {
-        MainUI mainUI = new MainUI(null);
        
-        // LoginUI
+        // LoginUI  -> mainUI  / SignUpUI
         SwingUtilities.invokeLater(() -> new LoginUI());
     }
 }
