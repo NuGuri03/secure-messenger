ff03598995b07028b16b7d5cb8452b0056fe874e
bss138 <tomorrowmgm1314@gmail.com>
2025-05-21 11:16:52 +0900

    Feat/signup UI (#13)
    * feat: SingUpUI 틀 생성
    
    * feat: 비밀번호 조건 구현
    
    * feat: 한글 입력 방지
    
    * feat: 팝업창 수정
    
    * feat: 메시지 창 수정
    
    * feat: UI 크기 조절
    
    * feat: UI 배치관리자 설정
    
    * feat: customDislog 줄바꿈 추가
    
    * feat: ID확인 삭제
    
    * feat: ID확인 삭제
    
    * chore: 정규식 수정
    
    * chore: 정규식 보완
    
    * feat: 안내문구 삽입, Ui 개선
    
    * chore: 안내문구 위치 조정
    
    * fix: id입력창 안내문구 버그 수정
    
    * fix: ID입력창 안내문구 버그 수정(ID 입력 후 다른 입력창으로 넘어가면 초기화되는 버그)
    
    * chore: 진입화면 변경
    
    * refactor: 불필요한 주석 및 import 제거
    
    * refactor: 전역변수 제거 및 코드 스타일 수정
    
    * refactor: 불필요한 panel 삭제
    
    * feat: 한국어 예외 처리 추가
    
    * feat: enter키 바인딩 추가
    
    ---------
    
    Co-authored-by: ssb <tomorrowmgm1314@gmaiil.com>
    Co-authored-by: NuGuri03 <sym2596@knu.ac.kr>

--- src/main/java/client/ui/SignUpUI.java
+++ src/main/java/client/ui/SignUpUI.java
@@ -1,14 +1,239 @@
 package client.ui;
 
+import client.ui.component.text.JTextFieldLimit;
+
 import javax.swing.*;
+import javax.swing.text.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
 
 public class SignUpUI extends BaseUI {
-    // TODO 1. 회원가입 UI 만들기 (아이디, 비밀번호, 성함, 생년월일 입력받게 해야함)
-    // TODO 2. 비밀번호에는 제약조건이 있어야함(제약조건을 어떻게 할지 의견을 내주셔야함)
-    /**
-     * 회원가입 UI 생성
-     */
     public SignUpUI() {
         super();
+
+        setTitle("회원가입");
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        Dimension minSize = new Dimension(430, 450);
+        setMinimumSize(minSize);
+        setLocationRelativeTo(null);
+
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(10, 10, 10, 10);
+
+        final Dimension TEXT_SIZE_DIMENSION = new Dimension(300, 30);
+        Font font = new Font("Pretendard", Font.PLAIN, 14);
+        this.setFont(font);
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.weightx = 0;
+        JLabel label = new JLabel("이름", SwingConstants.RIGHT);
+        gbc.anchor = GridBagConstraints.EAST;
+        add(label, gbc);
+
+        JTextField nameField = new JTextField("이름을 1자 이상 32자 이하로 입력해주세요");
+        nameField.setDocument(new JTextFieldLimit(32));
+        nameField.setPreferredSize(TEXT_SIZE_DIMENSION);
+        gbc.gridx = 1;
+        gbc.weightx = 1.0;
+        nameField.setFont(font);
+        add(nameField, gbc);
+
+        nameField.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (nameField.getText().equals("이름을 1자 이상 32자 이하로 입력해주세요")) {
+                    nameField.setText("");
+                    nameField.setForeground(Color.BLACK);
+                }
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (nameField.getText().isEmpty()) {
+                    nameField.setText("이름을 1자 이상 32자 이하로 입력해주세요");
+                    nameField.setForeground(Color.GRAY);
+                }
+            }
+        });
+
+        gbc.gridx = 0;
+        gbc.gridy++;
+        gbc.weightx = 0;
+        add(new JLabel("ID", SwingConstants.CENTER), gbc);
+
+        gbc.gridx = 1;
+        gbc.weightx = 1.0;
+        JTextField idField = new JTextField("소문자, 숫자, 특수기호(_ . -)만 사용할 수 있습니다");
+        idField.setPreferredSize(TEXT_SIZE_DIMENSION);
+        idField.setFont(font);
+        add(idField, gbc);
+        idField.setForeground(Color.GRAY);
+
+        idField.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (idField.getText().equals("소문자, 숫자, 특수기호(_ . -)만 사용할 수 있습니다")) {
+                    idField.setText("");
+                    idField.setForeground(Color.BLACK);
+                }
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+
+                String text = idField.getText();
+                if (text.isEmpty() || text.equals("소문자, 숫자, 특수기호(_ . -)만 사용할 수 있습니다")) {
+                    // placeholder 복구
+                    idField.setDocument(new PlainDocument());
+                    idField.setText("4~32자의 소문자, 숫자, 특수기호(_ . -)만 사용할 수 있습니다.");
+                    idField.setForeground(Color.GRAY);
+                }
+            }
+        });
+
+
+        // 비밀번호
+        gbc.gridx = 0;
+        gbc.gridy++;
+        gbc.weightx = 0;
+        add(new JLabel("비밀번호", SwingConstants.CENTER), gbc);
+
+        gbc.gridx = 1;
+        gbc.weightx = 1.0;
+        JPasswordField pwField = new JPasswordField();
+        pwField.setPreferredSize(TEXT_SIZE_DIMENSION);
+        pwField.setFont(font);
+        add(pwField, gbc);
+
+        gbc.insets = new Insets(0, 10, 3, 15);
+
+        // 메시지 라벨
+        gbc.gridx = 0;
+        gbc.gridy++;
+        gbc.gridwidth = 3;
+        gbc.weightx = 0;
+        gbc.anchor = GridBagConstraints.EAST;
+        JLabel messageLabel = new JLabel("비밀번호는 8자 이상 1024자 이하로 입력해주세요", SwingConstants.CENTER);
+        messageLabel.setFont(font);
+        add(messageLabel, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy++;
+        gbc.gridwidth = 3;
+        gbc.weightx = 0;
+        gbc.anchor = GridBagConstraints.EAST;
+        messageLabel = new JLabel("4~32자의 소문자, 숫자, 특수기호(_ . -)만 사용할 수 있습니다", SwingConstants.CENTER);
+        messageLabel.setFont(font);
+        add(messageLabel, gbc);
+
+        gbc.insets = new Insets(10, 10, 10, 10);
+
+        // 비밀번호 확인
+        gbc.gridx = 0;
+        gbc.gridy++;
+        gbc.gridwidth = 1;
+        gbc.weightx = 0;
+        add(new JLabel("비밀번호 확인", SwingConstants.CENTER), gbc);
+
+        gbc.gridx = 1;
+        gbc.weightx = 1.0;
+        JPasswordField pwcField = new JPasswordField();
+        pwcField.setPreferredSize(TEXT_SIZE_DIMENSION);
+        pwcField.setFont(font);
+        add(pwcField, gbc);
+
+        // 확인 버튼
+        gbc.gridx=0;
+        gbc.gridy++;
+        gbc.gridwidth = 2;
+        gbc.weightx = 0;
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.anchor = GridBagConstraints.CENTER;
+        JButton confirm = new JButton("확인");
+        confirm.setPreferredSize(new Dimension(100, 30));
+        confirm.setBackground(Color.WHITE);
+        confirm.setFont(font);
+        add(confirm, gbc);
+
+        confirm.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                signUp(nameField, idField, pwField, pwcField);
+            }
+        });
+
+        KeyStroke enterKey = KeyStroke.getKeyStroke("ENTER");
+        Action loginAction = new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                confirm.doClick();
+            }
+        };
+
+        pwcField.getInputMap(JComponent.WHEN_FOCUSED).put(enterKey, "confirm");
+        pwcField.getActionMap().put("confirm", loginAction);
+
+
+        setVisible(true);
+    }
+
+    private void signUp(JTextField nameField, JTextField idField, JPasswordField pwField, JPasswordField pwcField) {
+        String name = nameField.getText();
+        String id = idField.getText();
+        String password = new String(pwField.getPassword());
+        String confirmPassword = new String(pwcField.getPassword());
+
+        ArrayList<String> idList = new ArrayList<>();
+        idList.add("admin");
+
+        // 한국어 정규식
+        String koreanRegex = ".*[가-힣ㄱ-ㅎㅏ-ㅣ].*";
+
+        if (id.matches(koreanRegex) || password.matches(koreanRegex)) {
+            showCustomDialog("아이디와 비밀번호에는 한글을 포함할 수 없습니다");
+        } else if (name.isEmpty() || id.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+            showCustomDialog("모든 항목을 입력하세요");
+        } else if (!password.equals(confirmPassword)) {
+            showCustomDialog("비밀번호가 일치하지 않습니다");
+        } else if (!name.matches("^.{1,32}$")) {
+            showCustomDialog("이름은 1~32자로 작성하세요");
+        } else if (idList.contains(id)) {
+            showCustomDialog("중복된 아이디입니다");
+        } else if(!id.matches("^[a-z0-9_.\\-]{4,32}$")) {
+            showCustomDialog("ID는 소문자,숫자,특수기호(_ . -)4~32자로 작성하세요");
+        } else if(!password.matches("^[\\u0020-\\u007E]{8,1024}$")) {
+            showCustomDialog("비밀번호는 8글자 이상, 1024글자 이하로 작성하세요");
+        } else {
+            showCustomDialog("완료되었습니다");
+            this.dispose();
+        }
+    }
+
+    private void showCustomDialog(String message) {
+        JDialog dialog = new JDialog(this, "알림", true);
+        dialog.setSize(350, 150);
+        dialog.setFont(this.getFont());
+        dialog.getContentPane().setBackground(Color.WHITE);
+        dialog.setLocationRelativeTo(this);
+        dialog.setLayout(new BorderLayout());
+
+        JLabel label = new JLabel(message, SwingConstants.CENTER);
+        label.setFont(this.getFont());
+        label.setForeground(Color.BLACK);
+        dialog.add(label, BorderLayout.CENTER);
+
+        JButton okButton = new JButton("확인");
+        okButton.setFont(this.getFont());
+        okButton.setBackground(Color.WHITE);
+        okButton.addActionListener(e -> dialog.dispose());
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(okButton);
+        dialog.add(buttonPanel, BorderLayout.SOUTH);
+
+        dialog.setVisible(true);
     }
 }
