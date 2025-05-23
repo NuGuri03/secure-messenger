ab404af31d823efb69b07aed577057d7896b752d
NuGuri03 <sym2596@knu.ac.kr>
2025-05-23 10:39:05 +0900

    chore: 진입 화면 변경

--- src/main/java/client/ClientMain.java
+++ src/main/java/client/ClientMain.java
@@ -1,12 +1,9 @@
 package client;
 
-import client.ui.ChatUI;
 import client.ui.LoginUI;
 
 public class ClientMain {
     public static void main(String[] args) {
-//        new LoginUI();
-        ChatUI chat = new ChatUI("client");
-        chat.setVisible(true);
+        new LoginUI();
     }
 }
