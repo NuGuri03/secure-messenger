98e8945bf3a45ad894c21b8a9ff7a267858d223e
권혁주 <toturak1@gmail.com>
2025-05-25 20:02:18 +0900

    feat: ProfileUI base

--- src/main/java/client/ui/ProfileUI.java
+++ src/main/java/client/ui/ProfileUI.java
@@ -1,5 +1,26 @@
 package client.ui;
 
-public class ProfileUI {
-	// test
+import javax.swing.JFrame;
+import client.ui.component.button.UserIconButton;
+
+public class ProfileUI extends BaseUI{
+    private static final long serialVersionUID = 1L;
+
+    UserIconButton userIconButton;
+
+    public ProfileUI() {
+        userIconButton = new UserIconButton("/secure-messenger/src/main/resources/icons/default_profile.png", 32);
+
+        // 클릭 시 ProfileUI 오픈
+        userIconButton.addActionListener(e -> {
+            ProfileUI profile = new ProfileUI();
+            profile.setVisible(true);
+        });
+
+        add(userIconButton);
+
+        setSize(300, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
 }
