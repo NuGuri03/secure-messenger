93b3c506d9d1cfb7176d0d80bab4038e54ee24e5
권혁주 <toturak1@gmail.com>
2025-05-25 21:36:35 +0900

    feat: 배경 사진 추가, username 추가, 프로필 사진추가 fix: 프로필 사진 배경사진에 가려지고있음

--- src/main/java/client/ui/ProfileUI.java
+++ src/main/java/client/ui/ProfileUI.java
@@ -5,12 +5,11 @@ import javax.swing.*;
 
 public class ProfileUI extends BaseUI{
     private static final long serialVersionUID = 1L;
-    public ProfileUI() 
+    public ProfileUI(String username) 
     {
     	super();
 
-        setTitle("환영합니다");
-        setSize(400, 300);
+        setSize(400, 600);
         setResizable(true);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -19,23 +18,56 @@ public class ProfileUI extends BaseUI{
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(10, 10, 10, 10);
         
-        //Font mainFont = new Font("Pretendard", Font.PLAIN, 16);
+        Font mainFont = new Font("Pretendard", Font.PLAIN, 16);
         Font subFont = new Font("Pretendard", Font.PLAIN, 14);
         
+        
+        // 배경사진
+        ImageIcon BackIcon = new ImageIcon(getClass().getResource("/icons/user_profile.jpg"));
+        Image BackscaledImage = BackIcon.getImage().getScaledInstance(400, 400, Image.SCALE_SMOOTH);
+        JLabel BackLabel = new JLabel(new ImageIcon(BackscaledImage));
+
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.gridheight = 4; // 0~3행 병합
+        gbc.anchor = GridBagConstraints.CENTER;
+        add(BackLabel, gbc);
+        
+        // 프로필 사진
+        ImageIcon profileIcon = new ImageIcon(getClass().getResource("/icons/default_profile.png"));
+        Image scaledImage = profileIcon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+        JLabel profileLabel = new JLabel(new ImageIcon(scaledImage));
+        
+        profileLabel.setOpaque(false);
+ 
+        gbc.gridy = 3;
+        gbc.gridheight = 1;
+        gbc.anchor = GridBagConstraints.CENTER;
+        add(profileLabel, gbc);
+        
+        
+        // 프로필 이름
+        JLabel labelID = new JLabel(username);
+        labelID.setFont(mainFont);
+        gbc.gridy = 4;
+        gbc.gridheight = 1;
+        gbc.anchor = GridBagConstraints.CENTER;
+        add(labelID, gbc);
+
+        
+        
+        // 채팅 버튼
         JButton btnChat = new JButton("1 : 1 채팅");
         btnChat.setFont(subFont);
-        
-        gbc.gridx = 2;
-        gbc.gridy = 0;
-        gbc.weightx = 1.0;
-        gbc.weighty = 1.0;
+     
+        gbc.gridy = 5;
+        gbc.gridheight = 1;
         gbc.anchor = GridBagConstraints.SOUTH;
-        gbc.fill = GridBagConstraints.NONE;
-
+        
         add(btnChat, gbc);
         
         btnChat.addActionListener(e->{
-        	new ChatUI("홍길동");
+        	new ChatUI(username);
         });
         
         setVisible(true);
