bbc49f4909d9d4bbeb6c84dd85c171d757be7f0c
권혁주 <toturak1@gmail.com>
2025-05-25 21:03:55 +0900

    feat: 새 채팅 버튼 생성

--- src/main/java/client/ui/ProfileUI.java
+++ src/main/java/client/ui/ProfileUI.java
@@ -1,26 +1,44 @@
 package client.ui;
 
-import javax.swing.JFrame;
-import client.ui.component.button.UserIconButton;
+import java.awt.*;
+import javax.swing.*;
 
 public class ProfileUI extends BaseUI{
     private static final long serialVersionUID = 1L;
+    public ProfileUI() 
+    {
+    	super();
 
-    UserIconButton userIconButton;
-
-    public ProfileUI() {
-        userIconButton = new UserIconButton("/secure-messenger/src/main/resources/icons/default_profile.png", 32);
-
-        // 클릭 시 ProfileUI 오픈
-        userIconButton.addActionListener(e -> {
-            ProfileUI profile = new ProfileUI();
-            profile.setVisible(true);
-        });
-
-        add(userIconButton);
-
-        setSize(300, 300);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("환영합니다");
+        setSize(400, 300);
+        setResizable(true);
         setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(10, 10, 10, 10);
+        
+        //Font mainFont = new Font("Pretendard", Font.PLAIN, 16);
+        Font subFont = new Font("Pretendard", Font.PLAIN, 14);
+        
+        JButton btnChat = new JButton("1 : 1 채팅");
+        btnChat.setFont(subFont);
+        
+        gbc.gridx = 2;
+        gbc.gridy = 0;
+        gbc.weightx = 1.0;
+        gbc.weighty = 1.0;
+        gbc.anchor = GridBagConstraints.SOUTH;
+        gbc.fill = GridBagConstraints.NONE;
+
+        add(btnChat, gbc);
+        
+        btnChat.addActionListener(e->{
+        	new ChatUI("홍길동");
+        });
+        
+        setVisible(true);
+
     }
 }
